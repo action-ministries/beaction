041a1ee1a903d02bb0822192b4392b9209f3b7e9
Elijah Williams <eliwilliamsdesign@gmail.com>
2019-04-19 00:48:32 -0500

    update image

--- resources/views/church/master.blade.php
+++ resources/views/church/master.blade.php
@@ -14,7 +14,7 @@
     <meta property="og:url"           content="{{ url('/') }}" />
 	<meta property="og:type"          content="website" />
 	<meta property="og:title"         content="@yield('title') | ACTION Christian Center" />
-	<meta property="og:image"         content="{{ asset('church/img/pastorIke2.jpg') }}" />
+	<meta property="og:image"         content="{{ asset('church/img/church.jpg') }}" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('/church/css/app.css') }}" />
