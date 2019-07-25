414365b2e04473428de276e30d6f7b5c2beb1e73
Elijah Williams <eliwilliamsdesign@gmail.com>
2019-07-24 22:37:20 -0500

    update events page

--- resources/views/church/pages/events.blade.php
+++ resources/views/church/pages/events.blade.php
@@ -5,26 +5,6 @@
     <div class="jumbotron events bluebg text-center">
         <h1>Upcoming Events</h1>
     </div>
-    <!-- Church Picnic -->
-    <div class="row salmonbg event">
-        <div class="row">
-            <div class="col-lg-6 nopad">
-                <img src="https://www.bridgetonmo.com/ImageRepository/Document?documentID=321" class="img-fluid event__image" />
-            </div>
-            <div class="col-lg-6 nopad content event__content text-right">
-                <div class="wrap event__content-wrap">
-                    <h2 class="event__content-title">Annual Church Picnic<small class="event__content-title-date">July 13 (10am)</small></h2>
-                    <p>ACC-South is celebrating it's 3rd Anniversary and is inviting EVERYONE to the church's picnic celebration. There will be games, BBQ, desserts, a DJ and an all-around wholesome good time. If you're reading this... you're invited.
-                    <address class="event__content-address">
-                        Gentry Park<br />
-                        11454 Ann Mar Dr<br />
-                        Bridgeton, MO 63044
-                    </address>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Revival -->
     <div class="row bluebg flip event">
         <div class="row">
